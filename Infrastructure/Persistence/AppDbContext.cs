1fcdaa9c0714077b09e483f3feea6d8c34665c58
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-06-24 08:50:36 +0200

    infrastructure
    creation of persistence proj
    add persistence proj to sln

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Application;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ Infrastructure/Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using Application;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b =>
+                    b.MigrationsAssembly("WebApi")));
+
+            services.AddScoped<IAppDbContext>(provider => provider.GetService<AppDbContext>());
+        }
+    }
+}
